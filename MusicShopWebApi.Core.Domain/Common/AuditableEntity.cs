5744a5724eb30faa36e9f6af189f52d63929dc7f
Wojciech Jezierzanski <wojciech.jezierzanski@gmail.com>
2021-05-06 15:34:46 +0200

    Add property to AudiatableEntity

--- MusicShopWebApi.Core.Domain/Common/AuditableEntity.cs
+++ MusicShopWebApi.Core.Domain/Common/AuditableEntity.cs
@@ -8,5 +8,9 @@ namespace MusicShopWebApi.Core.Domain.Common
 {
     class AuditableEntity
     {
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LastModifiedDate { get; set; }
     }
 }
