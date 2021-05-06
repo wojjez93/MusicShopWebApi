6395830911c3a9a96c9a8f91df200052cdd2aae1
Wojciech Jezierzanski <wojciech.jezierzanski@gmail.com>
2021-05-06 17:13:15 +0200

    Add Category and Picture class

---
+++ MusicShopWebApi.Core.Domain/Entieties/Picture.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MusicShopWebApi.Core.Domain.Entieties
+{
+    public class Picture
+    {
+        public int Id { get; set; }        
+        public string ImageUrlNum1 { get; set; }
+        public string ImageUrlNum2 { get; set; }
+        public string ImageUrlNum3 { get; set; }
+        public string ImageUrlNum4 { get; set; }
+        public string ImageUrlNum5 { get; set; }
+        public string ImageUrlNum6 { get; set; }
+        public string ImageUrlnum7 { get; set; }
+        public string ImageUrlNum8 { get; set; }       
+    }
+}
