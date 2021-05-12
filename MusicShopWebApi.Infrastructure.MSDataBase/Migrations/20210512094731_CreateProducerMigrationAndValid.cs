7b6cbbdef59bf9150c9ac106c0c1ba469712a9ea
Wojciech Jezierzanski <wojciech.jezierzanski@gmail.com>
2021-05-12 12:00:26 +0200

    AddProducerMigration

---
+++ MusicShopWebApi.Infrastructure.MSDataBase/Migrations/20210512094731_CreateProducerMigrationAndValid.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicShopWebApi.Infrastructure.MSDataBase.Migrations
+{
+    public partial class CreateProducerMigrationAndValid : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Producers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AddressUrl = table.Column<string>(type: "nvarchar(2100)", maxLength: 2100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Producers");
+        }
+    }
+}
