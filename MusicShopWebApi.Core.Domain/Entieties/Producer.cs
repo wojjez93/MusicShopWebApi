2741e78cd13e1d311cf33557fa28e3fe118c6841
Wojciech Jezierzanski <wojciech.jezierzanski@gmail.com>
2021-06-01 17:05:23 +0200

    Add several test to Producer

--- MusicShopWebApi.Core.Domain/Entieties/Producer.cs
+++ MusicShopWebApi.Core.Domain/Entieties/Producer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,39 @@ namespace MusicShopWebApi.Core.Domain.Entieties
     public class Producer
     {
         [Key]
-        public int Id { get; set; }
+        public int Id { get; private set; }
         [Required]
         [MaxLength(50)]
-        public string Name { get; set; }
+        public string Name { get; private set; }
         public string Description { get; set; }
         [MaxLength(2100)]
         public string AddressUrl { get; set; }
         [MaxLength(2100)]
         public string ImageLogoUrl { get; set; }
+
+        private Producer()
+        {
+
+        }
+        public Producer(int id, string name, string description, string addressUrl, string imageLogoUrl)
+        {
+            Id = id;
+            Name = name;
+            Description = description;
+            AddressUrl = addressUrl;
+            ImageLogoUrl = imageLogoUrl;
+        }
+        public void ChangeName(string newName)
+        {
+            if (string.IsNullOrEmpty(newName))
+            {
+                throw new ArgumentNullException(newName);
+            }
+            if (newName.Length >= 50)
+            {
+                throw new ArgumentOutOfRangeException(newName);
+            }
+            Name = newName;
+        }
     }
 }
---
+++ MusicShopWebApi.Core.Test.Units/Entieties/ProducerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using MusicShopWebApi.Core.Domain;
+using MusicShopWebApi.Core.Domain.Entieties;
+
+namespace MusicShopWebApi.Core.Test.Units.Entieties
+{
+    public class ProducerTests
+    {
+        private Producer producer;
+        
+        [TestCase("ddddd")]
+        [TestCase("ola sndbajd")]        
+        public void ChangeName_(string newName)
+        {            
+            producer.ChangeName(newName);
+
+            var exception = producer.Name;
+
+            exception.ShouldBe(newName);           
+        }
+        [Test]
+        public void ChangeName_NameIsNotNullAndEmpty()
+        {
+            Assert.Throws<ArgumentNullException>(()=>producer.ChangeName(""));
+        }
+        [Test]
+        public void ChangeName_NameIsTooLong()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => producer.ChangeName("udsghfgdhgdscghscghsddcgshcsdchsdcghdscdghssdgfjhsgchjdsgjhdscghjsgcjhdsgcjsgdhjsdghjdgfjsdgfjhdgfjdsgfjsdgfjsdgfhjfgsfgudsghfgdhgdscghscghsddcgshcsdchsdcghdscdghssdgfjhsgchjdsgjhdscghjsgcjhdsgcjsgdhjsdghjdgfjsdgfjhdgfjdsgfjsdgfjsdgfhjfgsfgdgfsdgfjhgfhdgfsdjgfjdsgfsdgcjhdsgfjdsgfjdsgfjgfjhsgfjsdgfjsdgfsdgfsdjgfdsjfgjsgfsjagfdgfdsjgfjdsgfdsjgfjsdgfjdsgf"));
+        }
+        public ProducerTests()
+        {
+            producer = new Producer(1, "Logo", "ABCD", "", "");
+        }        
+    }
+}
